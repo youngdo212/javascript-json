fc15631f7f4919b646a5a151784761c0e3b9b0fb
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-24 15:32:51 +0900

    object 분석

--- app.js
+++ app.js
@@ -1,39 +1,56 @@
 "use strict";
 var rl = require('./rl.js');
+var polyfill = require('./polyfill.js');
 
-Number.isInteger = Number.isInteger || function (value) {
-    return typeof value === 'number' &&
-        isFinite(value) &&
-        Math.floor(value) === value;
-};
-
-function jsonParse(json) {
-    var array = JSON.parse(json);
-    var type = {
-        "숫자": 0,
-        "문자열": 0,
-        "부울": 0
-    };
-
-    array.forEach(function (element) {
-        typeof (element) === "boolean" ? type.부울++: Number.isInteger(element) ? type.숫자++ : type.문자열++;
+function printResult(type, parentType) {
+    var total = Object.values(type).reduce(function (pre, cur) {
+        return pre + cur;
     });
-
-    var results = "총 " + array.length + "개의 데이터 중에 ";
+    var results = "총 " + total + "개의 " + parentType + " 데이터 중에 ";
 
     Object.keys(type).filter(function (key) {
         return type[key] !== 0
     }).map(function (key) {
-        results += key + " " + type[key] +"개 ";
+        results += key + " " + type[key] + "개 ";
     });
 
     results += "포함되어 있습니다.";
 
     console.log(results);
 }
-rl.question('분석할 JSON 데이터를 입력하세요.\n', (answer) => {
 
-    jsonParse(answer);
+var type = {
+    "문자열": 0,
+    "숫자": 0,
+    "부울": 0,
+    "객체": 0
+};
 
-    rl.close();
-});
+function arrayParse(array) {
+    array.forEach(function (element) {
+        typeof (element) === "boolean" ? type.부울++: Number.isInteger(element) ? type.숫자++ : element.toString() === "[object Object]" ? type.객체++ : type.문자열++;
+    });
+    printResult(type, "배열");
+}
+
+function objectParse(object) {
+    Object.keys(object).forEach(function (key) {
+        var element = object[key];
+        typeof (element) === "boolean" ? type.부울++: Number.isInteger(element) ? type.숫자++ : type.문자열++;
+    });
+    printResult(type, "객체");
+}
+
+function jsonParse(json) {
+    var parsedJSON = JSON.parse(json);
+
+    parsedJSON.toString() === "[object Object]" ? objectParse(parsedJSON) : arrayParse(parsedJSON);
+}
+
+jsonParse('[ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }, { "name" : "YOUN JISU", "alias" : "crong", "level" : 4, "married" : true } ]');
+// rl.question('분석할 JSON 데이터를 입력하세요.\n', (answer) => {
+
+//     jsonParse(answer);
+
+//     rl.close();
+// });
---
+++ polyfill.js
@@ -0,0 +1,59 @@
+(function (global) {
+
+    Number.isInteger = Number.isInteger || function (value) {
+        return typeof value === 'number' &&
+            isFinite(value) &&
+            Math.floor(value) === value;
+    };
+
+    if (!Array.isArray) {
+        Array.isArray = function (arg) {
+            return Object.prototype.toString.call(arg) === '[object Array]';
+        };
+    }
+
+    if (!Object.keys) {
+        Object.keys = (function () {
+            'use strict';
+            var hasOwnProperty = Object.prototype.hasOwnProperty,
+                hasDontEnumBug = !({
+                    toString: null
+                }).propertyIsEnumerable('toString'),
+                dontEnums = [
+                    'toString',
+                    'toLocaleString',
+                    'valueOf',
+                    'hasOwnProperty',
+                    'isPrototypeOf',
+                    'propertyIsEnumerable',
+                    'constructor'
+                ],
+                dontEnumsLength = dontEnums.length;
+
+            return function (obj) {
+                if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+                    throw new TypeError('Object.keys called on non-object');
+                }
+
+                var result = [],
+                    prop, i;
+
+                for (prop in obj) {
+                    if (hasOwnProperty.call(obj, prop)) {
+                        result.push(prop);
+                    }
+                }
+
+                if (hasDontEnumBug) {
+                    for (i = 0; i < dontEnumsLength; i++) {
+                        if (hasOwnProperty.call(obj, dontEnums[i])) {
+                            result.push(dontEnums[i]);
+                        }
+                    }
+                }
+                return result;
+            };
+        }());
+    }
+
+})(typeof self !== "undefined" && self || typeof window !== "undefined" && window || this);
