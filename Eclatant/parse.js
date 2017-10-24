c8db6eee824c3c7ee8f376b9f7e9f8c450cffa49
Jeewhan <distinct19@gmail.com>
2017-10-24 20:52:16 +0900

    Chore: Prettier

--- Eclatant/parse.js
+++ Eclatant/parse.js
@@ -6,15 +6,15 @@
 // [ 10, "jk", 4, "314", 99, "crong", false ]
 // 총 7개의 데이터 중에 문자열 3개, 숫자 3개, 부울 1개가 포함되어 있습니다.
 
-var readline = require('readline');
+var readline = require("readline");
 
 var rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-rl.question('분석할 JSON 데이터를 입력하세요.\n', function(input) {
-  var inputToArray = input.slice(2, -2).split(', ');
+rl.question("분석할 JSON 데이터를 입력하세요.\n", function(input) {
+  var inputToArray = input.slice(2, -2).split(", ");
   var str = 0;
   var num = 0;
   var bool = 0;
@@ -24,11 +24,11 @@ rl.question('분석할 JSON 데이터를 입력하세요.\n', function(input) {
     var typeofResult = typeof v;
     var len = inputToArray.length;
 
-    if (v === 'true' || v === 'false') {
+    if (v === "true" || v === "false") {
       bool += 1;
     } else if (!isNaN(parseInt(v))) {
       num += 1;
-    } else if (typeof v === 'string') {
+    } else if (typeof v === "string") {
       str += 1;
     }
   });
@@ -46,7 +46,7 @@ rl.question('분석할 JSON 데이터를 입력하세요.\n', function(input) {
   }
 
   console.log(
-    `총 ${inputToArray.length}개의 데이터 중에 ${tempArr.join(', ')}가 포함되어 있습니다.`,
+    `총 ${inputToArray.length}개의 데이터 중에 ${tempArr.join(", ")}가 포함되어 있습니다.`
   );
 
   rl.close();
