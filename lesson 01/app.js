0b4b1d5a0d1d6e34f7036c2534a4218e175cc47f
crispynap <crispynap@gmail.com>
2017-11-02 16:09:47 +0900

    change main()

--- lesson 01/app.js
+++ lesson 01/app.js
@@ -3,6 +3,7 @@
 var readline = require('./config/readline')();
 var common = require('./src/common');
 var parser = require('./src/jsonParser');
+var reader = require('./src/jsonReader');
 
 
 (function () {
@@ -10,8 +11,13 @@ var parser = require('./src/jsonParser');
   readline.prompt();
 
   readline.on('line', function (insert) {
-    var dataCount = parser.parse(insert);
+    var parsedData;
+    var dataCount;
+
+    parsedData = parser.parseJson(insert);
+    dataCount = reader.countType(parsedData);
     common.messages.jsonCount(dataCount);
+
     common.messages.waitInsert();
     readline.prompt();
   });
