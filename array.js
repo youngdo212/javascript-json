e1a42941f6bcb8d47e7fefc13aa9b7431fad0dd7
wfe-luigi <kdh3979@naver.com>
2017-11-06 16:32:14 +0900

    기능 함수화 및 버그수정
    readStr readNum readBool 함수를 생성
    ->해당하는 값의 읽기시작지점을 전달하여 값을 획득
    
    true,false 입력에 대해 다른값(trueee,fasd 등)을 정확히 검사

--- array.js
+++ array.js
@@ -1,107 +1,103 @@
-/*1차
-allread 상태에서 시작 
-띄어쓰기 외에 특정문자의 받음 . 
-    
-    숫자 -> 그다음에 숫자 , ] , 띄어쓰기 옴, state는 inNumber ->  num에 있는 값 읽기모드, 띄어쓰기가 온 경우는 ,나 ]만 올수잇음
-    처음시작할때 pos1에 index저장,  ,가 오거나 ] 가 오면 pos2에 인덱스 저장 -> 추출기로 값 추출
-
-    " -> '를 제외한 모든 문자의 받음. state 는  inString 
-    처음시작할때 pos1에 index저장 , "가 오면 pos2에 index를 저장 -> 추출기로 값 추출
-    
-    ] 가 오면 종료
-
-*/
-//pos2 가 저장될때 -> 새로운배열.push(pos1~pos2) //pos1부터 그냥 temp에 push 해도 되지 않을까?
-/*2차
-state 0 시작전 [를 만나면 state 1로 전환하고 continue
-state 1 시작 읽기모드 all read -> test[i] == " "(공백) 이면 continue 
-다음것 만났을때 if indexof num , elseif test[i] == "" , elseif t or f  , pos1에 i를 저장
-state 2 숫자를 읽기시작 다음은 num안에서 옴 -> 공백 , ] 가 오면 pos2에 i-1을 저장, num++
-state 3 스트링을 읽기시작 -> "가 오면 state 1로 수정 하고 pos2에 i-1을 저장, str++
-state 4 true false 읽기 시작 -> 공백이 오면 1로 수정하고 pos2에 i-1을, bol++ 
-*/
-const de = 0;
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin, output: process.stdout
 });
 
-var test = '["ab","g",true,"bac",123]';
 const num = "0123456789";
-var bool = ["true", "false"];
+const endvalue = [" ", ",", "]", "}"];
 
+function readStr(input, i) { //문자열과 다음 i 반환, 에러는 -1 반환
+    var pos = i;
+    for (; i < input.length; i++) {
+        if (input[i] === '"') {
+            return [i, input.slice(pos, i)];
+        }
+        else if (input[i] === "'") {
+            console.log("문자열 입력오류 pos ", i, " : ", input[i]);
+            return -1; //process.exit();
+        }
+    }
+}
+
+function readNum(input, i) { //숫자와 다음 i 반환 , 에러는 -1 반환
+    var pos = i;
+    for (; i < input.length; i++) {
+        if (endvalue.indexOf(input[i]) !== -1) {
+            return [i, Number(input.slice(pos, i))];
+        }
+        else if (num.indexOf(input[i]) !== -1) { }
+        else {
+            console.log("숫자 읽기 에러 : ", input[i]);
+            return -1;
+        }
+    }
+}
+
+function readBool(input, i) {//bool값과 다음 i 반환, 에러는 -1 반환
+    var pos = i;
+    for (; i < input.length; i++) {
+        if (endvalue.indexOf(input[i]) !== -1) {
+            tmp = input.slice(pos, i);
+            if (tmp === 'true') return [i, true];
+            else if (tmp === 'false') return [i, false];
+            else {
+                console.log("t/f 읽기 에러 : ", input[i]);
+                return -1;
+            }
+        }
+    }
+}
 function token(answer) {
     var temp = [], i, length = answer.length;
     var count = { num: 0, str: 0, bol: 0 };
-    var pos1 = null, pos2 = null, state = 0;
+    var pos1 = null, state = "READY";
     for (i = 0; i < length; i++) {
-        de && console.log(answer[i], " state : ", state);
-        if (state === 0) {
+        if (state === "READY") {
             if (answer[i] === "[") {
-                de && console.log("읽기시작");
-                state = 1;
+                state = "READ";
             }
         }
-        else if (state === 1) {
-            if (answer[i] === " ") {
-                de && console.log("공백!");
+        else if (state === "READ") {
+            if (answer[i] === "'") {
+                console.log("에러 : ' 입력!");
             }
+
             else if (num.indexOf(answer[i]) !== -1) { //숫자가 들어옴
-                de && console.log("숫자시작");
-                pos1 = i;
-                state = 2;
+                var getnum = readNum(answer, i); //잘못된 숫자면 false를 반환
+                if (getnum === -1) {
+                    return console.log("숫자 읽기 에러 종료!");
+                }
+                else {
+                    i = getnum[0];
+                    state = "READ"; count.num++;
+                    temp.push(getnum[1]);
+                }
             }
             else if (answer[i] === '"') {
-                de && console.log("문자시작");
-                pos1 = i;
-                state = 3;
+                var getstr = readStr(answer, i + 1);
+                if (getstr === -1) {
+                    return console.log("문자 읽기 에러 종료!");
+                }
+                else {
+                    i = getstr[0];
+                    state = "READ", count.str++;
+                    temp.push(getstr[1]);
+                }
             }
             else if (answer[i] === "t" || answer[i] === "f") {
-                de && console.log("t/f 시작");
-                pos1 = i;
-                state = 4;
-            }
-        }
-        else if (state === 2) {
-            de && console.log("숫자읽기중");
-            if (answer[i] === "]") {
-                de && console.log("숫자 탈출!");
-                pos2 = i; count.num++;
-                temp.push(Number(answer.slice(pos1, pos2)));
-                state = 0;
-            }
-            else if (answer[i] === ',') {
-                de && console.log("숫자 탈출, 읽기시작!");
-                pos2 = i;
-                temp.push(Number(answer.slice(pos1, pos2)));
-                state = 1; count.num++;
-            }
-        }
-        else if (state === 3) {
-            de && console.log("문자읽기중");
-            if (answer[i] === '"') {
-                de && console.log("문자 탈출!");
-                pos2 = i;
-                temp.push(answer.slice(pos1 + 1, pos2));
-                state = 1; count.str++;
-            }
-        }
-        else if (state === 4) {
-            de && console.log("true,false 읽기중");
-            if (answer[i] === " " || answer[i] === "e") {
-                de && console.log("true false 탈출!");
-                pos2 = i + 1;
-                if (answer.slice(pos1, pos2) == "true") {
-                    temp.push(true);
+                var getbool = readBool(answer, i);
+                if (getbool === -1) {
+                    return console.log("true,false 읽기 에러 종료!");
                 }
-                else temp.push(false);
-                state = 1; count.bol++;
+                else {
+                    i = getbool[0];
+                    state = "READ", count.bol++;
+                    temp.push(getbool[1]);
+                }
             }
         }
-        de && console.log("------------");
     }
-    console.log(temp);
-    console.log(count);
+    console.log(temp); console.log(count);
 }
 rl.question("분석할 JSON 데이터를 입력하세요 : ", function (answer) {
     token(answer);
