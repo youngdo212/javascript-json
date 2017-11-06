adb5dc3d2aa3c7d4888ff65ef8239994153a9267
crispynap <crispynap@gmail.com>
2017-11-06 12:13:09 +0900

    add: error

--- lesson 01/src/messages.js
+++ lesson 01/src/messages.js
@@ -1,20 +1,17 @@
 var util = require('./utils');
 var log = util.log;
 
-var common = (function () {
-  var messages = {
-    waitInsert: function () {
-      log('Please insert json data');
-    },
-    jsonCount: function (count) {
-      sumCount = count.string + count.number + count.bool;
-      log(`총 ${sumCount}개의 데이터 중에 문자열 ${count.string}개, 숫자 ${count.number}개, 부울 ${count.bool}개가 포함되어 있습니다.`);
-    },
-  };
+var messages = {
+  waitInsert: function () {
+    log('Please insert json data');
+  },
+  jsonCount: function (count) {
+    sumCount = count.string + count.number + count.bool;
+    log(`총 ${sumCount}개의 데이터 중에 문자열 ${count.string}개, 숫자 ${count.number}개, 부울 ${count.bool}개가 포함되어 있습니다.`);
+  },
+  waitInsert: function () {
+    log('Please insert json data');
+  },
+};
 
-  return {
-    messages
-  };
-})();
-
-module.exports = common;
+module.exports = messages;
