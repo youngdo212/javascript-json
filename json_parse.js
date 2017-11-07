04b927c39c09a46aedcc08755cb0d505f969ada5
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-11-07 14:08:32 +0900

    Add json_parse.js

--- json_parse.js
+++ json_parse.js
@@ -1,135 +1,153 @@
+var polyfill = require('./polyfill.js');
+
 var json_parse = (function () {
     "use strict";
 
+    var text;
     var at;
     var ch;
-    var escapee = {
-        "\"": "\"",
-        "\\": "\\",
-        "/": "/",
-        b: "\b",
-        f: "\f",
-        n: "\n",
-        r: "\r",
-        t: "\t"
-    };
-    var text;
+    var type;
 
-    var error = function (m) {
-
-
-        throw {
-            name: "SyntaxError",
-            message: m,
-            at: at,
-            text: text
-        };
+    function checkType(element) {
+        if (typeof (element) === "boolean")
+            type.부울++;
+        else if (Number.isInteger(element))
+            type.숫자++;
+        else if (Object.prototype.toString.apply(element) === "[object Object]")
+            type.객체++;
+        else if (Array.isArray(element))
+            type.배열++;
+        else
+            type.문자열++;
     };
 
-    var next = function (c) {
+    function printResult(parentType) {
+        var partial = [];
+        var total = 0;
+        Object.keys(type).filter(function (key) {
+            return type[key] !== 0
+        }).forEach(function (key) {
+            var value = type[key];
+            total += value;
+            partial.push(key + " " + value + "개");
+        });
+        console.log(`총 ${total}개의 ${parentType} 데이터 중에 ${partial.join(", ")}가 포함되어 있습니다.`);
+    };
 
+    function arrayParse(array) {
+        array.forEach(function (element) {
+            checkType(element);
+        });
+        printResult("배열");
+    };
 
-        if (c && c !== ch) {
-            error("Expected '" + c + "' instead of '" + ch + "'");
+    function objectParse(object) {
+        Object.keys(object).forEach(function (key) {
+            checkType(object[key]);
+        });
+        printResult("객체");
+    };
+
+    function array() {
+        var arr = [];
+        if (ch === "[") {
+            next("[");
+            white();
+            if (ch === "]") {
+                next("]");
+                return arr;
+            }
+            while (ch) {
+                arr.push(value());
+                white();
+                if (ch === "]") {
+                    next("]");
+                    return arr;
+                }
+                next(",");
+                white();
+            }
         }
-
-
-        ch = text.charAt(at);
-        at += 1;
-        return ch;
+        throw new Error("Bad array");
     };
 
-    var number = function () {
+    function object() {
+        var obj = {};
+        if (ch === "{") {
+            next("{");
+            white();
+            if (ch === "}") {
+                next("}");
+                return obj;
+            }
+            while (ch) {
+                var key = string();
+                white();
+                next(":");
+                obj[key] = value();
+                white();
+                if (ch === "}") {
+                    next("}");
+                    return obj;
+                }
+                next(",");
+                white();
+            }
+        }
+        throw new Error("Bad object");
+    };
 
+    function string() {
+        var str = "";
+        if (ch === "\"") {
+            next("\"");
+            white();
+            while (ch) {
+                str += ch;
+                next(ch);
+                if (ch === "\"") {
+                    next("\"");
+                    return str;
+                }
+            }
+        }
+        throw new Error("Bad string");
+    };
 
-        var value;
-        var string = "";
-
+    function number() {
+        var num = "";
         if (ch === "-") {
-            string = "-";
+            num = "-";
             next("-");
         }
-        while (ch >= "0" && ch <= "9") {
-            string += ch;
-            next();
+        while ("0" <= ch && ch <= "9") {
+            num += ch;
+            next(ch);
         }
-        if (ch === ".") {
-            string += ".";
-            while (next() && ch >= "0" && ch <= "9") {
-                string += ch;
-            }
-        }
-        if (ch === "e" || ch === "E") {
-            string += ch;
-            next();
-            if (ch === "-" || ch === "+") {
-                string += ch;
-                next();
-            }
-            while (ch >= "0" && ch <= "9") {
-                string += ch;
-                next();
-            }
-        }
-        value = +string;
-        if (!isFinite(value)) {
-            error("Bad number");
+        if (isFinite(num)) {
+            return parseInt(num, 10);
         } else {
-            return value;
+            throw new Error("Bad number");
         }
     };
 
-    var string = function () {
-
-
-        var hex;
-        var i;
-        var value = "";
-        var uffff;
-
-
-        if (ch === "\"") {
-            while (next()) {
-                if (ch === "\"") {
-                    next();
-                    return value;
-                }
-                if (ch === "\\") {
-                    next();
-                    if (ch === "u") {
-                        uffff = 0;
-                        for (i = 0; i < 4; i += 1) {
-                            hex = parseInt(next(), 16);
-                            if (!isFinite(hex)) {
-                                break;
-                            }
-                            uffff = uffff * 16 + hex;
-                        }
-                        value += String.fromCharCode(uffff);
-                    } else if (typeof escapee[ch] === "string") {
-                        value += escapee[ch];
-                    } else {
-                        break;
-                    }
-                } else {
-                    value += ch;
-                }
-            }
-        }
-        error("Bad string");
-    };
-
-    var white = function () {
-
-
-        while (ch && ch <= " ") {
-            next();
+    function value() {
+        white();
+        switch (ch) {
+            case "{":
+                return object();
+            case "[":
+                return array();
+            case "\"":
+                return string();
+            case "-":
+                return number();
+            default:
+                return ch >= "0" && ch <= "9" ? number() : word();
         }
     };
 
-    var word = function () {
-
-
+    function word() {
         switch (ch) {
             case "t":
                 next("t");
@@ -151,124 +169,40 @@ var json_parse = (function () {
                 next("l");
                 return null;
         }
-        error("Unexpected '" + ch + "'");
+        throw new Error("Bad word");
     };
 
-    var value;
-    var array = function () {
-
-
-        var arr = [];
-
-        if (ch === "[") {
-            next("[");
-            white();
-            if (ch === "]") {
-                next("]");
-                return arr;
-            }
-            while (ch) {
-                arr.push(value());
-                white();
-                if (ch === "]") {
-                    next("]");
-                    return arr;
-                }
-                next(",");
-                white();
-            }
+    function next(c) {
+        if (c && c !== ch) {
+            throw new Error("Bad next");
         }
-        error("Bad array");
+
+        ch = text.charAt(at);
+        at++;
+        return ch;
     };
 
-    var object = function () {
-
-
-        var key;
-        var obj = {};
-
-        if (ch === "{") {
-            next("{");
-            white();
-            if (ch === "}") {
-                next("}");
-                return obj;
-            }
-            while (ch) {
-                key = string();
-                white();
-                next(":");
-                if (Object.hasOwnProperty.call(obj, key)) {
-                    error("Duplicate key '" + key + "'");
-                }
-                obj[key] = value();
-                white();
-                if (ch === "}") {
-                    next("}");
-                    return obj;
-                }
-                next(",");
-                white();
-            }
-        }
-        error("Bad object");
-    };
-
-    value = function () {
-
-
-        white();
-        switch (ch) {
-            case "{":
-                return object();
-            case "[":
-                return array();
-            case "\"":
-                return string();
-            case "-":
-                return number();
-            default:
-                return (ch >= "0" && ch <= "9") ?
-                    number() :
-                    word();
+    function white() {
+        if (ch && ch === " ") {
+            next();
         }
     };
 
-
-    return function (source, reviver) {
-        var result;
-
+    return function (source) {
         text = source;
-        at = 0;
         ch = " ";
-        result = value();
+        at = 0;
+        type = {
+            문자열: 0,
+            숫자: 0,
+            부울: 0,
+            객체: 0,
+            배열: 0
+        };
         white();
-        if (ch) {
-            error("Syntax error");
-        }
-
-        return (typeof reviver === "function") ?
-            (function walk(holder, key) {
-                var k;
-                var v;
-                var val = holder[key];
-                if (val && typeof val === "object") {
-                    for (k in val) {
-                        if (Object.prototype.hasOwnProperty.call(val, k)) {
-                            v = walk(val, k);
-                            if (v !== undefined) {
-                                val[k] = v;
-                            } else {
-                                delete val[k];
-                            }
-                        }
-                    }
-                }
-                return reviver.call(holder, key, val);
-            }({
-                "": result
-            }, "")) :
-            result;
+        var result = value();
+        Array.isArray(result) ? arrayParse(result) : objectParse(result);
+        return result;
     };
 }());
 
