788ba552149c6cf5091ac732b2e94455868c9318
wfe-luigi <kdh3979@naver.com>
2017-11-12 04:24:51 +0900

    토큰 함수 객체화

--- merge.js
+++ merge.js
@@ -3,60 +3,61 @@ const rl = readline.createInterface({
     input: process.stdin, output: process.stdout
 });
 
-const num = "0123456789";
-const endvalue = [" ", ",", "]", "}"];
 var count = { num: 0, str: 0, bol: 0, arr: 0, obj: 0 };
+var length;
+
 function printError(token, position) {
     console.log("Unexpected token ", token, "at position ", position);
 }
-function tokenizer(input, i, to, str) {
-
-}
-function readStr(input, i) { //문자열과 다음 i 반환, 에러는 -1 반환
-    var pos = i;
-    for (; i < input.length; i++) {
-        if (input[i] === '"') {
-            return [i, input.slice(pos, i)];
+var tokenizer = {
+    num: "0123456789",
+    endvalue: [" ", ",", "]", "}"],
+    readStr: function (input, i) {//문자열과 다음 i 반환, 에러는 -1 반환
+        var pos = i;
+        for (; i < length; i++) {
+            if (input[i] === '"') {
+                return [i, input.slice(pos, i)];
+            }
         }
-        // else if (input[i] === "'") {
-        //     printError(input[i], i);
-        //     return -1; //process.exit();
-        // }
-    }
-    console.log("Unexpected end of JSON input");
-    process.exit();
-}
-function readNum(input, i) { //숫자와 다음 i 반환 , 에러는 -1 반환
-    var pos = i;
-    for (; i < input.length; i++) {
-        if (endvalue.indexOf(input[i]) !== -1) {
-            return [i, Number(input.slice(pos, i))];
-        }
-        else if (num.indexOf(input[i]) !== -1) { }
-        else {
-            printError(input[i], i);
-            return -1;
-        }
-    }
-}
-function readBool(input, i) {//bool값과 다음 i 반환, 에러는 -1 반환
-    var pos = i;
-    for (; i < input.length; i++) {
-        if (endvalue.indexOf(input[i]) !== -1) {
-            tmp = input.slice(pos, i);
-            if (tmp === 'true') return [i, true];
-            else if (tmp === 'false') return [i, false];
+        console.log("Unexpected end of JSON input");
+        process.exit();
+    },
+    readNum: function (input, i) { //숫자와 다음 i 반환 , 에러는 -1 반환
+        var pos = i;
+        for (; i < length; i++) {
+            if (this.endvalue.indexOf(input[i]) !== -1) {
+                return [i, Number(input.slice(pos, i))];
+            }
+            else if (this.num.indexOf(input[i]) !== -1) { }
             else {
-                printError(tmp, i);
+                printError(input[i], i);
                 return -1;
             }
         }
+    },
+    readBool: function (input, i) {//bool값과 다음 i 반환, 에러는 -1 반환
+        var pos = i;
+        for (; i < length; i++) {
+            if (this.endvalue.indexOf(input[i]) !== -1) {
+                tmp = input.slice(pos, i);
+                if (tmp === 'true') return [i, true];
+                else if (tmp === 'false') return [i, false];
+                else {
+                    printError(tmp, i);
+                    return -1;
+                }
+            }
+        }
     }
-}
+};
 function getObject(answer, start, nested) {
-    var temp = {}, i = 0, length = answer.length;
-    var depth = 0, state = "READY", key;
-    if (start != undefined) i = start;
+    var temp = [], i = 0, depth = 0
+    var state = "READY", key = null;
+
+    if (start !== undefined) {
+        i = start;
+    }
+
     for (; i < length; i++) {
         if (state === "READY") {
             if (answer[i] === "{") {
@@ -66,7 +67,7 @@ function getObject(answer, start, nested) {
         }
         else if (state == "READ_KEY") {
             if (answer[i] === '"') {
-                var getstr = readStr(answer, i + 1);
+                var getstr = tokenizer.readStr(answer, i + 1);
                 if (getstr === -1) {
                     return console.log("문자 읽기중 에러 종료!");
                 }
@@ -101,8 +102,8 @@ function getObject(answer, start, nested) {
                 state = "READ_NEXT";
 
             }
-            else if (num.indexOf(answer[i]) !== -1) { //숫자가 들어옴
-                var getnum = readNum(answer, i); //잘못된 숫자면 false를 반환
+            else if (tokenizer.num.indexOf(answer[i]) !== -1) { //숫자가 들어옴
+                var getnum = tokenizer.readNum(answer, i); //잘못된 숫자면 false를 반환
                 if (getnum === -1) {
                     return console.log("숫자 읽기 에러 종료!");
                 }
@@ -113,7 +114,7 @@ function getObject(answer, start, nested) {
                 }
             }
             else if (answer[i] === '"') { //문자열 읽기 시작
-                var getstr = readStr(answer, i + 1);
+                var getstr = tokenizer.readStr(answer, i + 1);
                 if (getstr === -1) {
                     return console.log("문자 읽기 에러 종료!");
                 }
@@ -124,12 +125,11 @@ function getObject(answer, start, nested) {
                 }
             }
             else if (answer[i] === "t" || answer[i] === "f") {
-                var getbool = readBool(answer, i);
+                var getbool = tokenizer.readBool(answer, i);
                 if (getbool === -1) {
                     return console.log("true,false 읽기 에러 종료!");
                 }
                 else {
-
                     i = getbool[0] - 1;
                     state = "READ_NEXT", count.bol++;
                     temp[key] = getbool[1];
@@ -166,11 +166,13 @@ function getObject(answer, start, nested) {
 }
 
 function getArray(answer, start, nested) {
-    var temp = [], i = 0, depth = 0, length = answer.length;
+    var temp = [], i = 0, depth = 0;
     var state = "READY";
+
     if (start !== undefined) {
         i = start;
     }
+
     for (; i < length; i++) {
         if (state === "READY") {
             if (answer[i] === "[") {
@@ -191,8 +193,8 @@ function getArray(answer, start, nested) {
             if (answer[i] === "'") {
                 console.log("Unexpected token ", answer[i], "at position ", i);
             }
-            else if (num.indexOf(answer[i]) !== -1) { //숫자가 들어옴
-                var getnum = readNum(answer, i); //잘못된 숫자면 false를 반환
+            else if (tokenizer.num.indexOf(answer[i]) !== -1) { //숫자가 들어옴
+                var getnum = tokenizer.readNum(answer, i); //잘못된 숫자면 false를 반환
                 if (getnum === -1) {
                     return console.log("숫자 읽기 에러 종료!");
                 }
@@ -202,7 +204,7 @@ function getArray(answer, start, nested) {
                 }
             }
             else if (answer[i] === '"') { //문자열읽기 시작
-                var getstr = readStr(answer, i + 1);
+                var getstr = tokenizer.readStr(answer, i + 1);
                 if (getstr === -1) {
                     return console.log("문자 읽기 에러 종료!");
                 }
@@ -213,7 +215,7 @@ function getArray(answer, start, nested) {
                 }
             }
             else if (answer[i] === "t" || answer[i] === "f") {
-                var getbool = readBool(answer, i);
+                var getbool = tokenizer.readBool(answer, i);
                 if (getbool === -1) {
                     return console.log("true,false 읽기 에러 종료!");
                 }
@@ -261,8 +263,10 @@ function getArray(answer, start, nested) {
     if (depth !== 0) { console.log("Unexpected end of JSON input"); process.exit() }
     return [temp, i];
 }
+
 rl.question("분석할 JSON 데이터를 입력하세요 : ", function (answer) {
     var result;
+    length = answer.length;
     if (answer[0] === "[") {
         result = [];
         result = getArray(answer);
