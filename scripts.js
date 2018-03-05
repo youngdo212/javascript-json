1e25f0c640410a2aacc989c190f776f0e05b8fcd
Dongwoo Lee <likedemian@me.com>
2018-03-05 16:43:11 +0900

    Fix: condition to object

--- scripts.js
+++ scripts.js
@@ -4,15 +4,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('분석할 JSON 데이터를 입력하세요. ', (answer) => {
-  let arr = answer.split(' ');
-  let res = [];
-  objects(arr, res);
-  types(res);
-  rl.close();
-});
 
-const objects = (arr, res) => {
+const init = () => {
+  rl.question('분석할 JSON 데이터를 입력하세요. ', (answer) => {
+    let arr = answer.split(' ');
+    let res = [];
+    arrays(arr, res);
+    console.log(result(res));
+    rl.close();
+  });
+}
+
+
+const arrays = (arr, res) => {
   if (arr[0] === '[' && arr[arr.length - 1] === ']') {
     for (let i = 1; i < arr.length - 1; i++) {
       arr[i].match(',') ? res.push(arr[i].replace(',', "")) : res.push(arr[i]);
@@ -21,19 +25,40 @@ const objects = (arr, res) => {
   return res;
 }
 
-const types = (res) => {
-  let strs = [];
-  let nums = [];
-  let bools = [];
 
-  for (let val of res) {
-    if (val.match('"')) {
-      strs.push(val.replace('"', "").replace('"', ""));      
-    } else if (!isNaN(Number(val))) {
-      nums.push(Number(val));      
-    } else if (val === 'true' || 'false') {
-      bools.push(val);
+const types = {
+  string: function (res) {
+    let strs = [];
+    for (let val of res) {
+      if (val.match('"')) {
+        strs.push(val.replace('"', "").replace('"', ""));
+      }
     }
+    return strs.length;
+  },
+  numbers: function (res) {
+    let nums = [];
+    for (let val of res) {
+      if (!isNaN(Number(val))) {
+        nums.push(Number(val));
+      }
+    }
+    return nums.length;
+  },
+  bools: function (res) {
+    let bools = [];
+    for (let val of res) {
+      if (val === "false" || val === "true") {
+        bools.push(val);
+      }
+    }
+    return bools.length;
   }
-  console.log("총 " + res.length + "개의 데이터 중에 문자열 " + strs.length + "개, 숫자 " + nums.length + "개, 부울 " + bools.length + "개가 포함되어 있습니다.");
 }
+
+const result = (res) => {
+  return "총 " + res.length + "개의 데이터 중에 문자열 " + types.string(res) + "개, 숫자 " + types.numbers(res) + "개, 부울 " + types.bools(res) + "개가 포함되어 있습니다.";
+}
+
+
+init();
