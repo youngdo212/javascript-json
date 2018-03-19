013bc48e8a9b05d12ddda74cc1a429e6f4e4e64e
Dongwoo Lee <likedemian@me.com>
2018-03-19 21:45:23 +0900

    Add: new json parser function

--- scripts.js
+++ scripts.js
@@ -1,262 +1,141 @@
-/* 
-분석할 JSON 데이터를 입력하세요.
-[ "name" : "KIM JUNG" ]
-> 지원하지 않는 형식을 포함하고 있습니다.
-
-{ "name" : "KIM JUNG' }
-> 지원하지 않는 형식을 포함하고 있습니다.
-
-분석할 JSON 데이터를 입력하세요.
-{ "name" : "KIM JUNG" "alias" : "JK" }
-> 지원하지 않는 형식을 포함하고 있습니다.
-
-분석할 JSON 데이터를 입력하세요.
-{ "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "children" : ["hana", "hayul", "haun"] }
-> 지원하지 않는 형식을 포함하고 있습니다.
-
-분석할 JSON 데이터를 입력하세요.
-{ "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }
-> 총 4개의 객체 데이터 중에 문자열 2개, 숫자 1개, 부울 1개가 포함되어 있습니다.
-
-분석할 JSON 데이터를 입력하세요.
-[ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }, { "name" : "YOON JISU", "alias" : "crong", "level" : 4, "married" : true } ]
-> 총 2개의 배열 데이터 중에 객체 2개가 포함되어 있습니다.
- */
-
-// let answer = '{ "name" : "KIM JUNG"}';
-// let answer = '{ "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married": true }';
-// let answer = '[ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }, { "name" : "YOUN JISU", "alias" : "crong", "level" : 4, "married" : true }, { "name" : "JUNG HO", "alias" : "honux","level" : 1, "married" : true }]'
-// let answer = '[ 10, "jk", 4, "314", 99, "name", "crong", false ]';
-
-
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let Objectkeys = [];
-let tempStr = '';
-let tempArr = [];
-let is_ready_to_input_Data;
-let message = {
-  init: `분석할 JSON 데이터를 입력하세요. \n`,
-  error: `지원하지 않는 형식을 포함하고 있습니다.`
+let flags = {
+  isObject: 0,
+  isArray: 0,
+  isStr: 0,
+  isNum: 0,
+  isBool: 0
 }
 
+let arr = []
 
+let cursor = -1;
+const init = () => {
+  while (++cursor < input.length) {
+    let char = input[cursor];
+    switch (char) {
+      case '[':
+        flags.isArray++;
+        continue;
 
-const errCheck = {
+      case '{':
+        flags.isObject++;
+        continue;
 
-  caseOfArrays(answer) {
-    return this._hasNoColon(answer) &&
-      this._hasQuotesEven(answer) &&
-      this._isSomethingInCommas(answer) ?
-      parseArrays(answer) : message.error;
-  },
+      case '}':
+        break;
 
-  caseOfObjectArray(answer) {
-    return this._leftBraceAfterBracket(answer) &&
-      this._hasQuotesEven(answer) &&
-      this._rightBracketAfterBrace(answer) ?
-      parseObjects(answer) : message.error;
-  },
+      case ']':
+        break;
 
-  caseOfObjects(answer) {
-    return this._hasQuotesEven(answer) &&
-      this._numberOfCommasAndColons(answer) &&
-      this._putColonOutsideOfKeys(answer) ?
-      parseValues(answer) : message.error;
-  },
+      case ' ':
+      case ',':
+        continue;
 
-  _hasNoColon(answer) {
-    return (answer.match(/\:/g) || []).length === 0;
-  },
+      case '"':
+        parseString();
+        continue;
 
-  _isSomethingInCommas(answer) {
-    return answer.match(/\,.\,/g) === null;
-  },
-
-  _leftBraceAfterBracket(answer) {
-    return answer.indexOf('{') > answer.indexOf('[');
-  },
-
-  _rightBracketAfterBrace(answer) {
-    return answer.indexOf(']') > answer.indexOf('}');
-  },
-
-  _hasQuotesEven(answer) {
-    return (answer.match(/\"/g) || []).length % 2 === 0;
-  },
-
-  _numberOfCommasAndColons(answer) {
-    return (answer.match(/\:/g) || []).length - (answer.match(/\,/g) || []).length === 1;
-  },
-
-  _putColonOutsideOfKeys(answer) {
-    Objectkeys.forEach(elem => {
-      console.log(elem);
-      return elem[elem.length - 1] === ":" ? elem : message.error
-    });
-  }
-
-}
-
-
-
-const init = (answer) => {
-  let braces = answer.indexOf('{') !== -1 && answer.indexOf('}') !== -1;
-  let brackets = answer.indexOf('[') !== -1 && answer.indexOf(']') !== -1;
-  brackets ? console.log(hasBrackets(answer, brackets, braces)) : console.log(hasNoBrackets(answer, brackets, braces));
-}
-
-const hasBrackets = (answer, brackets, braces) => {
-  return braces ? errCheck.caseOfObjectArray(answer) : errCheck.caseOfArrays(answer)
-}
-
-const hasNoBrackets = (answer, brackets, braces) => {
-  return braces ? errCheck.caseOfObjects(answer) : message.error;
-}
-
-const parseArrays = (answer) => {
-  let counts = {
-    total: 0,
-    number: 0,
-    string: 0,
-    boolean: 0
-  };
-
-  answer = answer.replace(/\[|\]/gi, '');
-  for (let elem of answer) {
-    tempStr += elem;
-  };
-
-  tempArr = tempStr.split(',').map(elem => {
-    return elem.trim();
-  });
-
-  tempArr.forEach(elem => {
-    counts.total++;
-    if (elem.indexOf('"') !== -1) {
-      counts.string++
-    } else
-    if (!isNaN(elem)) {
-      counts.number++
-    } else if (elem === 'true' || elem === 'false') {
-      counts.boolean++
-    }
-  })
-
-  return Object.is(counts.total, counts.string + counts.boolean + counts.number) ?
-    `총 ${counts.total}개의 데이터 중에 문자열 ${counts.string}개, 숫자 ${counts.number}개, 부울 ${counts.boolean}개가 포함되어 있습니다.` :
-    message.error;
-}
-
-const parseObjects = (answer) => {
-  let counts = {
-    arrays: 0,
-    objData: 0
-  }
-
-  for (let element of answer) {
-    if (element === '{') {
-      is_ready_to_input_Data = true;
-    };
-    if (is_ready_to_input_Data) {
-      tempStr += element;
-    };
-    if (element === '}') {
-      is_ready_to_input_Data = false;
-      tempArr.push(tempStr);
-      tempStr = '';
+      case 't':
+      case 'f':
+      case 'n':
+        parseBool();
+        continue;
+      default:
+        parseNumber();
     }
   }
-
-  counts.arrays = tempArr.length;
-
-  tempArr.forEach(elem => {
-    if (elem[0] === '{') {
-      counts.objData++
-    }
-  })
-
-  return `${counts.arrays}개의 배열 데이터 중 객체 ${counts.objData}개가 포함되어 있습니다.`
+  return flags;
 }
 
-const parseKeys = (tempArr) => {
-  let temps = [];
+console.log(init());
 
-  for (let elem of tempArr) {
-    for (let factor of elem) {
-      if (factor === '{' || factor === ',') {
-        is_ready_to_input_Data = true;
-      };
-      if (is_ready_to_input_Data) {
-        tempStr += factor;
-      };
-      if (factor === ':') {
-        is_ready_to_input_Data = false;
-        temps.push(tempStr);
-        tempStr = '';
+
+
+
+
+
+function parseString() {
+  cursor = cursor + 1;
+  if (input.charAt(cursor) === '"') {
+    cursor = cursor + 1;
+    if (/\,|\s|\}|\]/.test(input.charAt(cursor))) {
+      cursor = cursor + 1;
+      if (/[^\:]/.test(input.charAt(cursor))) {
+        flags.isStr++
       }
+    } else {
+      parseString();
     }
+  } else {
+    parseString();
   }
-  temps.forEach(elem => {
-    Objectkeys.push(elem.replace(/\{|\,/gi, '').replace(/\,}/gi, '').trim());
-  })
-  return Objectkeys;
 }
 
 
-const parseValues = (tempArr) => {
-  let temps = [];
-  let values = [];
-  let counts = {
-    total: 0,
-    number: 0,
-    string: 0,
-    boolean: 0
-  };
 
-  for (let elem of tempArr) {
-    for (let factor of elem) {
-      if (factor === ':') {
-        is_ready_to_input_Data = true;
-      };
+function parseNumber() {
+  cursor = cursor + 1;
+  if (/\,|\s|\}|\]/g.test(input.charAt(cursor))) {
+    flags.isNum++;
+  } else if (typeof (input.charAt(cursor) * 1) === 'number') {
+    parseNumber();
+  }
+}
 
-      if (is_ready_to_input_Data) {
-        tempStr += factor;
-      };
 
-      if (factor === ',' || factor === '}') {
-        is_ready_to_input_Data = false;
-        temps.push(tempStr);
-        tempStr = '';
+
+
+function parseBool() {
+  cursor = cursor + 1;
+  if (input.charAt(cursor) === 'r') {
+    cursor = cursor + 1;
+    if (input.charAt(cursor) === 'u') {
+      cursor = cursor + 1;
+      if (input.charAt(cursor) === 'e') {
+        flags.isBool++;
+      } else {
+        throw new Error("error입니다")
       }
+    } else {
+      throw new Error("error입니다")
+    }
+  } else if (input.charAt(cursor) === 'a') {
+    cursor = cursor + 1;
+    if (input.charAt(cursor) === 'l') {
+      cursor = cursor + 1;
+      if (input.charAt(cursor) === 's') {
+        cursor = cursor + 1;
+        if (input.charAt(cursor) === 'e') {
+          flags.isBool++;
+        } else {
+          throw new Error("error입니다")
+        }
+      } else {
+        throw new Error("error입니다")
+      }
+    } else {
+      throw new Error("error입니다")
+    }
+  } else if (input.charAt(cursor) === 'n') {
+    cursor = cursor + 1;
+    if (input.charAt(cursor) === 'u') {
+      cursor = cursor + 1;
+      if (input.charAt(cursor) === 'l') {
+        cursor = cursor + 1;
+        if (input.charAt(cursor) === 'l') {
+          flags.isBool++;
+        } else {
+          throw new Error("error입니다")
+        }
+      } else {
+        throw new Error("error입니다")
+      }
+    } else {
+      throw new Error("error입니다")
     }
   }
-
-  temps.forEach(elem => {
-    values.push(elem.replace(/\:\s/gi, '').replace(/\,|\}/gi, '').trim());
-  })
-  values.forEach(elem => {
-    counts.total++;
-    if (elem.indexOf('"') !== -1) {
-      counts.string++;
-    } else if (!isNaN(elem)) {
-      counts.number++;
-    } else if (elem === 'true' || elem === 'false') {
-      counts.boolean++;
-    }
-  })
-
-  return Object.is(counts.total, counts.string + counts.boolean + counts.number) ?
-    `총 ${counts.total}개의 객체 데이터 중에 문자열 ${counts.string}개, 숫자 ${counts.number}개, 부울 ${counts.boolean}개가 포함되어 있습니다.` :
-    message.error
+  cursor = cursor + 1;
+  if (!(/\,|\s|\}|\]/g.test(input.charAt(cursor)))) {
+    throw new Error("error입니다")
+  }
 }
-
-rl.question(message.init, (answer) => {
-
-  init(answer);
-  rl.close();
-});
