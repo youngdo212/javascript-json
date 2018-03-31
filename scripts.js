dc4d201bd864ebe6f5059a7a50b833b3f2c0f428
Dongwoo Lee <likedemian@me.com>
2018-03-31 15:33:59 +0900

    Add: New parse function

--- scripts.js
+++ scripts.js
@@ -1,225 +1,68 @@
-// let input = `[ 10, 21, 4, 314, 99, 0, 72 ]`;
-// let input = `[ 10, "jk", 4, "314", 99, "crong", false ]`;
-// let input = `{ "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }`;
-// let input = `[ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }, { "name" : "YOUN JISU", "alias" : "crong", "level" : 4, "married" : true } ]`
-// let input = `[ "name" : "KIM JUNG" ]`;
-// let input = `{ "name" : "KIM JUNG' }`;
-// let input = `{ "name" : "KIM JUNG" "alias" : "JK" }`;
-// let input = `{ "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "children" : [ "hana", "hayul", "haun" ] }`
+var input = `[1,2,[1,2,[],3],3]`;
+var data = `[{value: 1}, {value: 2}, {value: [{value: 1}, {value: 2}, {}, {value: 3}]}, {value:3}]`
+var char;
+var stack = 0;
+var res = [];
 
-let input = `[ { "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "married" : true }, { "name" : "YOON JISU", "alias" : "crong", "level" : 4, "married" : true } ]`
-let input = `{ "name" : "KIM JUNG", "alias" : "JK", "level" : 5, "children" : [ "hana", "hayul", "haun" ] }`;
+function myParse(val) {
+  var cursor = -1;
+  var arr = [0];
+  var obj = [];
+  val = val.trim();
 
-let char = null;
-let parseData = {
-  data: {
-    isObject: 0,
-    isArray: 0
-  },
-
-  value: {
-    isStr: 0,
-    isNum: 0,
-    isBool: 0,
-    isObject: 0,
-    isArray: 0
-  },
-
-  flags: {
-    isObject: false,
-    isArray: false
-  },
-
-  objects: {
-    isKey: 0,
-    isValue: 0
+  if (val[0] === '[' && val[val.length - 1] === ']') {
+    val = val.substring(1, val.length - 1).trim();
   }
-}
-
-let cursor = -1;
-const init = () => {
-  while (++cursor < input.length) {
-    char = input[cursor];
+  while (++cursor < val.length) {
+    char = val[cursor];
     switch (char) {
-
       case '[':
-        if ((input.charAt(cursor) === '[') && (cursor === 0)) {
-          parseData.data.isArray++;
-          cursor++
-        } else {
-          parseArray('[');
+        stack++;
+        continue;
+      case ']':
+        stack--;
+        continue;
+      case ',':
+        if (stack !== 0) {
+          break;
+        } else if (stack === 0) {
+          arr.push(cursor);
         }
         continue;
-
-      case '{':
-        if (input.charAt(0) === '{' && input.charAt(input.length - 1) === '}') {
-          parseData.data.isObject++;
-        } else {
-          parseObject('{')
-        }
-        continue;
-
-      case ' ':
-        continue;
-
-      case '"':
-        parseString();
-        continue;
-
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        parseNumber();
-        continue;
-
-      case 't':
-        parseBool('true');
-        continue;
-      case 'f':
-        parseBool('false');
-        continue;
-      case 'n':
-        parseBool('null');
-        continue;
-
-      default:
     }
   }
-  return parseData;
-}
 
-
-function isBlank() {
-  if (input.charAt(cursor).match(/\s+/g)) {
-    cursor++;
-    return isBlank();
-  }
-}
-
-
-function parseArray(char) {
-  if (cursor !== 0 && input.charAt(cursor) === char) {
-    cursor++;
-    parseData.flags.isArray = true;
-    parseData.value.isArray++;
-  } else {
-    parseArray(char);
-  }
-}
-
-function parseObject(char) {
-  if (input.charAt(cursor) === char) {
-    char++;
-    parseData.flags.isObject = true;
-    parseData.value.isObject++;
-  } else {
-    parseObject(char);
-  }
-}
-
-function parseString() {
-  cursor++;
-  if (input.charAt(cursor) === '"') {
-    cursor++;
-    isBlank();
-    if (input.charAt(cursor) === '"') {
-      throw new Error('지원하지 않는 형식을 포함하고 있습니다');
-    }
-
-    if (input.charAt(cursor) === ',') {
-      if (!parseData.value.isArray && !parseData.value.isObject) {
-        parseData.value.isStr++;
-      }
-    }
-
-    // if (input.charAt(cursor) === ']') {
-    //   parseData.value.isArray++;
-    // }
-
-    if (input.charAt(cursor) === ':') {
-      if (parseData.flags.isObject === false) {
-        if (parseData.data.isArray) {
-          throw new Error('지원하지 않는 형식을 포함하고 있습니다');
-        }
-      }
-      parseData.objects.isKey++;
-      cursor++;
-    }
-
-    if (cursor === input.length - 1) {
-      parseData.value.isStr++;
-    }
-
-  } else if (input.charAt(cursor) === "'") {
-    cursor++;
-    isBlank();
-    if (input.charAt(cursor) === "," || input.charAt(cursor) === "}") {
-      throw new Error('지원하지 않는 형식을 포함하고 있습니다');
+  for (var i = 0; i < arr.length; i++) {
+    debugger;
+    if (val.slice(arr[i], arr[i + 1])[0] !== ',') {
+      obj.push(val.slice(arr[i], arr[i + 1]).trim());
     } else {
-      parseString();
+      obj.push(val.slice(arr[i], arr[i + 1]).substring(1).trim());
     }
-  } else {
-    parseString();
   }
-}
 
-function parseValue() {
-  cursor++;
-  if (input.charAt(cursor) === '[') {} else {
-    parseValue();
+  res.push(obj);
+  for (var i = 0; i < obj.length; i++) {
+    if (obj[i][0] === '[') {
+      myParse(obj[i]);
+    }
   }
+  return res
 }
 
 
-function parseNumber() {
-  cursor++;
-  isBlank();
-  if (/\,|\]|\}/g.test(input.charAt(cursor))) {
-    return parseData.value.isNum++;
-  } else if (typeof (input.charAt(cursor) * 1) === 'number') {
-    parseNumber();
+
+function secondParse(val) {
+  for (var i = 0; i < val.length; i++) {
+    for (var j = 0; j < val[i].length; j++) {
+      if (val[i][j][0] === '[') {
+        val[i][j] = val[i + 1];
+      }
+    }
   }
+  var answer = val[0]
+  return answer;
 }
 
 
-function parseBool(bools) {
-  if (input.slice(cursor, cursor + bools.length) === bools) {
-    return parseData.value.isBool++
-      // } else {
-      // throw new Error('지원하지 않는 형식을 포함하고 있습니다');
-  };
-}
-
-
-init();
-
-
-
-let total = parseData.value.isStr + parseData.value.isNum + parseData.value.isBool;
-
-
-const message = {
-  array: `총 ${total}개의 데이터 중에 숫자 ${parseData.value.isNum}개, 문자열 ${parseData.value.isStr}개, 부울 ${parseData.value.isBool}개가 포함되어 있습니다.`,
-  object: `총 ${parseData.objects.isKey}개의 객체 데이터 중에 문자열 ${parseData.value.isStr}개, 숫자 ${parseData.value.isNum}개, 부울 ${parseData.value.isBool}개, 배열 ${parseData.value.isArray}가 포함되어 있습니다.`,
-  objectInArray: `총 ${parseData.value.isObject + parseData.value.isArray}개의 배열 데이터 중에 객체 ${parseData.value.isObject}개와 배열 ${parseData.value.isArray}개가 포함되어 있습니다.`
-}
-
-
-
-
-if (parseData.data.isArray) {
-  if (parseData.value.isObject || parseData.value.isArray) {
-    console.log(message.objectInArray);
-  } else {
-    console.log(message.array);
-  }
-} else if (parseData.data.isObject) {
-  console.log(message.object);
-}
+console.log(secondParse(myParse(input)));
