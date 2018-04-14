f7a3d7c4d83a2275d7127533111d74239ea233df
youngdo212 <nsa06031@gmail.com>
2018-04-14 17:24:05 +0900

    refactor: 중첩된 배열 파싱을 위해 로직 변경(계층화)
    arrayparser를 깊이에 따라 계층화 하도록 변경

--- 180413_arrayParser.js
+++ 180413_arrayParser.js
@@ -1,27 +1,46 @@
 function ArrayParser(str){
-  let result = {}
+  const result = {};
+  let step = 0;
+  let temp = '';
   for(let i = 0; i < str.length; i++){
-    if('1234567890'.indexOf(str[i]) !== -1){
-      result.type = 'number';
-      result.value = result.value ? result.value + str[i] : str[i];
-      result.child = [];
+    if(str[i] === '['){
+      result[step] = step === 0 ? ++step : result[step].concat(++step);
+      result[step] = [];
     }
-    else if('[]'.indexOf(str[i]) !== -1){
-      result.type = 'array';
-      result.child = str.slice(1, str.length-1).split(',')
-      .reduce((acc, curr) => [...acc, ArrayParser(curr.trim())], []);
-      break;
+    else if(str[i] === ']'){
+      result[step] = result[step].concat(temp || []);
+      temp = '';
+      --step;
+    }
+    else if(str[i] === ','){
+      result[step] = result[step].concat(temp || []);
+      temp = '';
+    }
+    else if(str[i] === ' '){
+      continue;
+    }
+    else{
+      temp += str[i];
     }
   }
+  result[step] = result[step] || temp;
+  
   return result;
 }
 
-let s1 = '[12, 14, 15]';
-console.log(ArrayParser(s1));
+let s1 = '[12, [14, 55], 15]';
+let s2 = '[1, [55, 3]]'
+let s3 = '[1, [[2]]]'
+let s4 = '[123,[22,23,[11,[112233],112],55],33]';
+let s5 = '12345'
+
+let result = ArrayParser(s1);
+console.log(result);
+
 /*
-{type: 'array',
-child: 
- [ { type: 'number', value: '12', child: [] },
-   { type: 'number', value: '14', child: [] },
-   { type: 'number', value: '15', child: [] } ] }
+{
+  0: 1,
+  1: ['12', 2, '15'],
+  2: ['14, '55']
+}
 */
