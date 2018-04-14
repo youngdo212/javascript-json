335b3b458bba9b8d86d9c394d4a6553e69b15729
youngdo212 <nsa06031@gmail.com>
2018-04-14 21:05:48 +0900

    fix: 같은 depth에 두 개 이상의 배열이 가능하게 수정

--- 180413_arrayParser.js
+++ 180413_arrayParser.js
@@ -1,19 +1,20 @@
 function ArrayParser(str){
   const result = {};
-  let step = 0;
+  let depth = 0;
   let temp = '';
   for(let i = 0; i < str.length; i++){
     if(str[i] === '['){
-      result[step] = step === 0 ? ++step : result[step].concat(++step);
-      result[step] = [];
+      result[++depth] = [];
     }
     else if(str[i] === ']'){
-      result[step] = result[step].concat(temp || []);
+      result[depth] = result[depth].concat(temp || []);
+      result[depth-1] = result[depth-1] ? result[depth-1].concat([result[depth]]) : result[depth];
+      delete result[depth];
+      depth--;
       temp = '';
-      --step;
     }
     else if(str[i] === ','){
-      result[step] = result[step].concat(temp || []);
+      result[depth] = result[depth].concat(temp || []);
       temp = '';
     }
     else if(str[i] === ' '){
@@ -23,7 +24,7 @@ function ArrayParser(str){
       temp += str[i];
     }
   }
-  result[step] = result[step] || temp;
+  result[depth] = result[depth] || temp;
   
   return result;
 }
@@ -33,14 +34,7 @@ let s2 = '[1, [55, 3]]'
 let s3 = '[1, [[2]]]'
 let s4 = '[123,[22,23,[11,[112233],112],55],33]';
 let s5 = '12345'
+let s6 = '[1,3,[1,2],4,[5,6]]'
 
 let result = ArrayParser(s1);
 console.log(result);
-
-/*
-{
-  0: 1,
-  1: ['12', 2, '15'],
-  2: ['14, '55']
-}
-*/
