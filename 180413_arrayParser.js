f788580dfb2a27072d8e404db51af5f096d7cd2c
youngdo212 <nsa06031@gmail.com>
2018-04-16 13:55:22 +0900

    style: 변수명 변경(last > lastStack)

--- 180413_arrayParser.js
+++ 180413_arrayParser.js
@@ -21,11 +21,11 @@ function ArrayParser(str){
       if(tempValue) stack[stack.length-1].push(new DataStucture('number', tempValue));
       tempValue = '';
       const child = stack.pop();
-      const last = stack.pop();
-      const lastData = last.pop();
+      const lastStack = stack.pop();
+      const lastData = lastStack.pop();
       lastData.pushChild(child);
-      last.push(lastData);
-      stack.push(last);
+      lastStack.push(lastData);
+      stack.push(lastStack);
     }
     else if(str[i] === ','){
       if(tempValue) stack[stack.length-1].push(new DataStucture('number', tempValue));
@@ -41,6 +41,8 @@ function ArrayParser(str){
   return tempValue ? new DataStucture('number', tempValue) : stack.pop().pop();
 }
 
+
+
 let testcase1 = '[12, [14, 55], 15]';
 let testcase2 = '[1, [55, 3]]'
 let testcase3 = '[1, [[2]]]'
@@ -48,5 +50,5 @@ let testcase4 = '[123,[22,23,[11,[112233],112],55],33]';
 let testcase5 = '12345'
 let testcase6 = '[1,3,[1,2],4,[5,6]]'
 
-let result = ArrayParser(testcase5);
+let result = ArrayParser(testcase6);
 console.log(JSON.stringify(result, null, 2));
